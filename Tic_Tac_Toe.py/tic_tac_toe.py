78b18ac6cad570df6ddd96333a16484b683700a1
Ecob1 <edgarcobian@live.com>
2022-01-13 12:41:28 -0800

    Adding main function

--- Tic_Tac_Toe.py/tic_tac_toe.py
+++ Tic_Tac_Toe.py/tic_tac_toe.py
@@ -5,15 +5,16 @@ board= [
 ]
 
 # This function will create the board
-def print_board(board):
+def main(board):
     for row in board:
         for slot in row:
             print(f"{slot} ", end="")
         print()
 
-print_board(board)
+main(board)
 
-def quit(user_quit):
+# If user want to quit the game
+def user_quit(user_quit):
     if user_input.lower() == "q":
         print("Thanks for playing")
         return True
@@ -27,19 +28,21 @@ def check_input(user_input):
     # Checking if its 1 - 9
     if not bounds(user_input): return False
     return True
-
+# To check if user will enter a number only
 def isnum(user_input):
     if not user_input.isnumeric():
         print("This is not a valid number")
         return False
     else: return True
 
+# This will check if user will enter a number between 1 to 9
 def bounds(user_input):
     if user_input > 9 or user_input < 1:
         print("This number is out of bounds")
         return False
     else: return True
 
+# this function will check if row and col hasn't been check
 def istaken(coords, board):
     row = coords[0]
     col = coords[1]
@@ -56,9 +59,9 @@ def coordinates(user_input):
     
 
 while True:
-    print_board(board)
-    user_input = input('Please enter a position 1 through 9 or enter \"q"\ to quit:')
-    if quit(user_input): break
+    main(board)
+    user_input = input('Please enter a position 1 through 9 or enter \"q"\ to quit: ')
+    if user_quit(user_input): break
     if not check_input(user_input):
         print("Please try again")
         continue
@@ -68,5 +71,8 @@ while True:
     if istaken(coords, board):
         print("Please try again.")
         continue 
+    
+if __name__ == "__main__":
+    main()
 
 
